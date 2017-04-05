b411dd878378d427f989ebcfcc80dc623c4ca581
Ridvan Orsvuran <orsvuran@geoazur.unice.fr>
2017-04-05 11:42:14 +0200

    add the dd_meas script

---
+++ pypaw/bins/dd_measure_adjoint_asdf.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env pythoiin
+# -*- coding: utf-8 -*-
+"""Script for dd adjoint source calculation
+
+:copyright:
+   Ridvan Orsvuran (orsvuran@geoazur.unice.fr), 2017
+:license:
+    GNU Lesser General Public License, version 3 (LGPLv3)
+    (http://www.gnu.org/licenses/lgpl-3.0.en.html)
+"""
+
+from __future__ import division, absolute_import
+from __future__ import print_function, unicode_literals
+
+
+import argparse
+from .utils import load_json, load_yaml
+
+from pypaw.doubledifference import calc_measures
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Adjoint source calculation for double difference")
+    parser.add_argument('-f', action='store', dest='path_file', required=True,
+                        help="path file")
+    parser.add_argument('-p', action='store', dest='param_file', required=True,
+                        help="param file")
+    parser.add_argument('-v', action='store_true', dest='verbose',
+                        help="verbose flag")
+    args = parser.parse_args()
+
+    params = load_yaml(args.param_file)
+    paths = load_json(args.path_file)
+    calc_measures(paths, params)
+
+
+if __name__ == "__main__":
+    main()
