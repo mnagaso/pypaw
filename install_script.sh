bac08f1f0e255ba2a235dd6135b4f1d7d1ba0363
Masaru Nagaso <mnsaru22@gmail.com>
2024-02-27 16:45:15 -0700

    --no-binary causing an error

--- install_script.sh
+++ install_script.sh
@@ -3,18 +3,18 @@
 # 2. git
 # 3. mpi
 # 4. HDF5 (parallel) installed
+# (please check the INSTALL.md for more details)
 
 export HDF5_DIR="/usr/lib/x86_64-linux-gnu/hdf5/openmpi/"
 
 conda env create -f environment.yml
 conda activate pypaw
-#pip install -r requirements.txt
 
 CC=mpicc pip install --no-binary=mpi4py mpi4py #==3.1.2
 #CC=mpicc HDF5_MPI=ON HDF5_DIR="$TACC_HDF5_DIR" pip install --no-binary=h5py h5py==3.5.0
-CC=mpicc HDF5_MPI=ON HDF5_DIR="$HDF5_DIR" pip install --no-binary=h5py h5py==3.5.0
+#CC=mpicc HDF5_MPI=ON HDF5_DIR="$HDF5_DIR" pip install --no-binary=h5py h5py==3.5.0
 
 ## or from source
-#rm -rf ./h5py && git clone https://github.com/h5py/h5py.git && cd h5py && HDF5_MPI=ON CC=mpicc pip install . && cd .. && rm -rf h5py
+rm -rf ./h5py && git clone https://github.com/h5py/h5py.git && cd h5py && HDF5_MPI=ON CC=mpicc pip install . && cd .. && rm -rf h5py
 
 pip install -e .
