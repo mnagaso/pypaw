99baffc306be144aa2b724f23b7bc38a04d2b949
Masaru Nagaso <mnsaru22@gmail.com>
2024-03-03 22:32:10 -0600

    add env file for frontera

--- install_script.sh
+++ install_script.sh
@@ -9,9 +9,19 @@
 arch_type=$(uname -m)
 
 # export for some general installation, but still need to check the path by yourself
-export HDF5_DIR="/usr/lib/${arch_type}-linux-gnu/hdf5/openmpi/"
+#export HDF5_DIR="/usr/lib/${arch_type}-linux-gnu/hdf5/openmpi/"
+export HDF5_DIR="/opt/apps/intel19/impi19_0/phdf5/1.10.4/x86_64/"
 
-conda env create -f environment.yml
+# modify PATH to prioritize miniconda3 path
+CONDA_bin="/home1/09793/mnagaso/miniconda3/bin"
+#CONDA_dir = conda info | grep "env location" | cut -d ":" -f 2
+export PATH="$CONDA_bin:$PATH"
+
+# make sure that PYTHONPATH is empty (module load command may add some path)
+export PYTHONPATH=""
+
+#conda env create -f environment.yml
+conda env create -f environment_frontera.yml
 conda activate pypaw
 
 CC=mpicc pip install --no-binary=mpi4py mpi4py #==3.1.2
